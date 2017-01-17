bf4e5c100c5ce4575eb3c4ddd81d7d09b0266771
Merichu Duncan <chue_3@yahoo.com>
2017-01-17 15:22:39 -0500

    Added new commits

--- housesexample/House.cs
+++ housesexample/House.cs
@@ -8,5 +8,27 @@ namespace housesexample
 {
     class House
     {
+        private string color;
+        private int rooms;
+        private int size;
+        private int doors;
+
+        public House(string color, int rooms, int size, int doors)
+        {
+            this.color = color;
+            this.rooms = rooms;
+            this.size = size;
+            this.doors = doors;
+        }
+
+
+        public void Paint(string color)
+        {
+            this.color = color;
+        }
+        public string CurrentColor()
+        {
+            return color;
+        }
     }
 }
--- housesexample/Program.cs
+++ housesexample/Program.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace housesexample
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            House house1 = new House("Green", 4, 1000, 3);
+            //house1.Paint("Green");
+            //house1.NumberofRooms(4);
+            //house1.SetSize(1000);
+            //house1.NUmberofDoors(3);
+            Console.WriteLine(house1);
+            
         }
     }
 }
